38bd17ccf9c438f9a713e2ea7caf52beb60f0222
sarwar-jahin-port <sarwarjahinpersonal@gmail.com>
2023-08-18 16:39:01 +0600

    category section added with responsiveness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { TopBanner } from './TopBanner/TopBanner'
 import { SpecialOffer } from './SpecialOffer/SpecialOffer'
+import { Category } from './Category/Category'
 
 export const Home = () => {
   return (
     <>
       <TopBanner></TopBanner>
       {/* Special Offers and Category section */}
-      <div className='md:flex'>
+      <div className='md:flex md:gap-20'>
         <div>
-          <h1 className='text-3xl text-center'>Special Offers</h1>
+          <h1 className='text-3xl text-center my-3'>Special Offers</h1>
           <SpecialOffer></SpecialOffer>
         </div>
-
+        <Category></Category>
       </div>
     </>
   )
